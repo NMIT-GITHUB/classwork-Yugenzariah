d7610e3278e71aa995b852826385522284e5e3fb
Keith Jhaeron Cayatoc <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2025-02-19 15:24:37 +1300

    Created simple app

--- basic/app.js
+++ basic/app.js
@@ -1,3 +1,15 @@
-const express = require('express');
-const log = require('morgan');
-const http = require('http');
+const express = require('express'); // Import the express module
+const log = require('morgan'); // Import the morgan module
+const http = require('http'); // Import the http module
+
+const app = express(); // Create an Express app
+
+app.use((req, res, next) => { // Request, Response, Next functions
+    const minute = new Date().getMinutes(); // Create a constant variable that holds the current time minutes
+    if(minute % 2 === 0) { // If the current time minutes are divisible by 2
+        next(); // Call the next function
+    }else{ 
+        res.statusCode = 403; // Set the status code to 403
+        res.end('Not authorized'); // End the response with a message
+    }
+});
